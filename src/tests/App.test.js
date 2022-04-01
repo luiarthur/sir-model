fda83d2ddec8b8aad2490e8a106fa7db147e477d
Arthur Lui <luiarthur@gmail.com>
2022-04-01 13:58:21 -0600

    Now using createRoot in index.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { App } from '../App';
 
-test('renders learn react link', () => {
+test("Renders sir model header", () => {
   render(<App />);
   const linkElement = screen.getByText(/SIR Model/i);
   expect(linkElement).toBeInTheDocument();
